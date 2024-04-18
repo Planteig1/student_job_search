7c7df6e593b94d13971c354cbda70796c301b2e4
Planteig <Planteig17@hotmail.com>
2024-04-18 09:38:46 +0200

    Added random files

---
+++ app/src/main/java/com/example/student_job_search_app/ui_models/home_screen/asd.kt
@@ -0,0 +1,4 @@
+package com.example.student_job_search_app.ui_models.home_screen
+
+class asd {
+}
---
+++ app/src/main/java/com/example/student_job_search_app/ui_models/liked_screen/asd.kt
@@ -0,0 +1,4 @@
+package com.example.student_job_search_app.ui_models.liked_screen
+
+class asd {
+}
